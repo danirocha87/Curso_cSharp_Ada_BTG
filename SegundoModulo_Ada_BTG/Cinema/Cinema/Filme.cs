c64e4ce555f7e97b7bb8182c3919f114e7f64a78
daniela oliveira <danioliver_17@hotmail.com>
2023-09-03 19:10:27 -0300

    Exercicio Cinema

---
+++ SegundoModulo_Ada_BTG/Cinema/Cinema/Filme.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema
+{
+    public class Filme
+    { 
+        public string Titulo { get; set;}
+        public int Ano { get;set;}
+        public int Duracao { get; set;}
+
+        public Filme(string titulo, int ano, int duracao)
+        {
+            Titulo = titulo;
+            Ano = ano;
+            Duracao = duracao;
+        
+        }
+    
+    }
+}
---
+++ SegundoModulo_Ada_BTG/Cinema/Cinema/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Cinema
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Filme filme = new Filme("Titanic", 2000, 222);
+
+            Sala sala = new Sala() { quantidadeCadeiras = 100, Numero = 10 };
+
+            Sessao sessao = new Sessao(sala, new DateTime(2023, 09, 02, 17, 20, 0));
+
+            sessao.AddFilme(filme);
+            Console.WriteLine(sessao);
+        }
+    }
+}
